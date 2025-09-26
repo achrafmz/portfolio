bc7e527879774af7bc268b00dbd9e7cbb9cd4dd1
achrafmz <achrafmazouz50@gmail.com>
2025-09-26 01:18:47 +0100

    hggf

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
          <>
          <Home />
       <Helmet>
-        <meta name="google-site-verification" content="google-site-verification=DGAXCNMKmD-u8gwwzyCqgufjWauFP-QXwbonYo8lqOc" />
-      </Helmet>
+  <meta name="google-site-verification" content="DGAXCNMKmD-u8gwwzyCqgufjWauFP-QXwbonYo8lqOc" />
+</Helmet>
       {/* Le reste de ton app */}
     </>
   );
