17ba585dbd8570e3ecd463c2284bf679b42dd5f8
achrafmz <achrafmazouz50@gmail.com>
2025-09-26 01:09:35 +0100

    hggf

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
+import { Helmet } from "react-helmet";
+
 import Home from "./pages/Home";
 function App() {
   return (
-      <Home />
+      
+         <>
+         <Home />
+      <Helmet>
+        <meta name="google-site-verification" content="google-site-verification=DGAXCNMKmD-u8gwwzyCqgufjWauFP-QXwbonYo8lqOc" />
+      </Helmet>
+      {/* Le reste de ton app */}
+    </>
   );
 }
 
